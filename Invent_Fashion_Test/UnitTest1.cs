db67dbc1e0225465289eca41c5043efc837efd09
Sujith Ashok <77700637+Sujith-Ashok@users.noreply.github.com>
2021-07-30 07:38:49 -0400

    Created and configured Library class

---
+++ Invent_Fashion_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Invent_Fashion_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
